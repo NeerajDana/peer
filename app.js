efd38be639da84a62505774eb4940ce1b8e56c8e
Neeraj Dana <neerajdana9@gmail.com>
2018-07-29 21:34:02 +0530

    my peer server

---
+++ app.js
@@ -0,0 +1,28 @@
+var express  = requrie('express')
+var path = require('path')
+var https = require('http')
+var errorhandler = require('errorhandler')
+var cors = require('cors')
+var ExpressPeerServer = require('peer').ExpressPeerServer
+
+var option = {
+    debug : true ,
+    key: 'codeshow'
+}
+var app = express()
+var server = http.createServer(app);
+
+var port = process.env.PORT || '3000';
+
+app.set('port' , port)
+
+app.use(cors());
+app.use(express.static(path.join(__dirname , 'public')));
+app.use('/peerjs' , ExpressPeerServer(server , option));
+app.use(errorhandler());
+
+process.on('uncaughtException' , function(exc){
+    console.error(exc);
+})
+
+server.listen(port)
